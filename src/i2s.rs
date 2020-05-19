aacb71598aa3021d8cd4d66bb8054b8a17ab8c58
Diego Barrios Romero <eldruin@gmail.com>
2020-05-19 22:31:29 +0200

    Refactor for compatibility with WriteIter impl

--- src/i2s.rs
+++ src/i2s.rs
@@ -70,45 +70,14 @@ macro_rules! impl_i2s_write {
                 left_words: &'w [$word_ty],
                 right_words: &'w [$word_ty],
             ) -> Result<(), Self::Error> {
-                let mut left = OutBitStream::new(left_words.iter());
-                let mut right = OutBitStream::new(right_words.iter());
-                match self.mode {
-                    Mode::I2s => {
-                        self.set_ws_low()?;
-                        // The very first time this will be missing one bit if WS was set to high.
-                        // However, we cannot know about the previous call or the previous pin status.
-                        loop {
-                            for _ in 0..($bit_count - 1) {
-                                match left.next() {
-                                    None => return Ok(()),
-                                    Some(bit) => self.try_write_bit(bit)?,
-                                }
-                            }
-                            self.set_ws_high()?;
-                            self.try_write_bit(left.next().unwrap_or(false))?; // last left bit
-
-                            for _ in 0..($bit_count - 1) {
-                                self.try_write_bit(right.next().unwrap_or(false))?;
-                            }
-                            self.set_ws_low()?;
-                            self.try_write_bit(right.next().unwrap_or(false))?; // last right bit
-                        }
-                    }
-                    Mode::LeftJustified => loop {
-                        self.set_ws_low()?;
-                        for _ in 0..$bit_count {
-                            match left.next() {
-                                None => return Ok(()),
-                                Some(bit) => self.try_write_bit(bit)?,
-                            }
-                        }
-
-                        self.set_ws_high()?;
-                        for _ in 0..$bit_count {
-                            self.try_write_bit(right.next().unwrap_or(false))?;
-                        }
-                    },
+                for (left_word, right_word) in left_words.iter().zip(right_words.iter()) {
+                    self.try_write_words(
+                        *left_word as $raw_ty,
+                        *right_word as $raw_ty,
+                        $bit_count,
+                    )?;
                 }
+                Ok(())
             }
         }
     };
@@ -124,6 +93,58 @@ where
     SD: OutputPin<Error = EP>,
     TIMER: CountDown + Periodic,
 {
+    fn try_write_words<W>(
+        &mut self,
+        left_word: W,
+        right_word: W,
+        bit_count: u8,
+    ) -> Result<(), Error<EP>>
+    where
+        W: core::ops::Shr<u8, Output = W>
+            + core::ops::BitAnd<Output = W>
+            + PartialEq
+            + From<u8>
+            + Copy,
+    {
+        let mut left = OutBitStream::new(left_word, bit_count);
+        let mut right = OutBitStream::new(right_word, bit_count);
+        match self.mode {
+            Mode::I2s => {
+                self.set_ws_low()?;
+                // The very first time this will be missing one bit if WS was set to high.
+                // However, we cannot know about the previous call or the previous pin status.
+                for _ in 0..(bit_count - 1) {
+                    match left.next() {
+                        None => return Ok(()),
+                        Some(bit) => self.try_write_bit(bit)?,
+                    }
+                }
+                self.set_ws_high()?;
+                self.try_write_bit(left.next().unwrap_or(false))?; // last left bit
+
+                for _ in 0..(bit_count - 1) {
+                    self.try_write_bit(right.next().unwrap_or(false))?;
+                }
+                self.set_ws_low()?;
+                self.try_write_bit(right.next().unwrap_or(false))?; // last right bit
+            }
+            Mode::LeftJustified => {
+                self.set_ws_low()?;
+                for _ in 0..bit_count {
+                    match left.next() {
+                        None => return Ok(()),
+                        Some(bit) => self.try_write_bit(bit)?,
+                    }
+                }
+
+                self.set_ws_high()?;
+                for _ in 0..bit_count {
+                    self.try_write_bit(right.next().unwrap_or(false))?;
+                }
+            }
+        }
+        Ok(())
+    }
     fn try_write_bit(&mut self, bit: bool) -> Result<(), Error<EP>> {
         if bit {
             self.sd.set_high().map_err(Error::Pin)?;
@@ -148,57 +169,39 @@ where
 }
 
 #[derive(Debug)]
-struct OutBitStream<WI, WU> {
-    data: WI,
-    word_offset: usize,
-    current_word: WU,
-    bit_offset: usize,
+struct OutBitStream<W> {
+    word: W,
+    bit_offset: u8,
+    bit_count: u8,
 }
 
-impl<WI, WU> OutBitStream<WI, WU>
-where
-    WU: From<u8>,
-{
-    fn new(data: WI) -> Self {
+impl<W> OutBitStream<W> {
+    fn new(word: W, bit_count: u8) -> Self {
         OutBitStream {
-            data,
-            word_offset: 0,
-            current_word: 0.into(),
+            word,
             bit_offset: 0,
+            bit_count,
         }
     }
 }
 
-macro_rules! impl_iterator_obs {
-    ($wi:ty, $wu:ty, $word_bit_count:expr) => {
-        impl<'a, WI> Iterator for OutBitStream<WI, $wu>
-        where
-            WI: Iterator<Item = &'a $wi>,
-        {
-            type Item = bool;
+impl<W> Iterator for OutBitStream<W>
+where
+    W: core::ops::Shr<u8, Output = W> + core::ops::BitAnd<Output = W> + PartialEq + From<u8> + Copy,
+{
+    type Item = bool;
 
-            #[inline]
-            fn next(&mut self) -> Option<Self::Item> {
-                if self.bit_offset == $word_bit_count {
-                    self.bit_offset = 0;
-                }
-                if self.bit_offset == 0 {
-                    match self.data.next() {
-                        Some(value) => self.current_word = *value as $wu,
-                        None => return None,
-                    }
-                }
-                self.bit_offset += 1;
-                // MSB first
-                Some(((self.current_word >> ($word_bit_count - self.bit_offset)) & 1) != 0)
-            }
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.bit_offset == self.bit_count {
+            None
+        } else {
+            self.bit_offset += 1;
+            // MSB first
+            Some(((self.word >> (self.bit_count - self.bit_offset)) & 1.into()) != 0.into())
         }
-    };
+    }
 }
-
-impl_iterator_obs!(i8, u8, 8);
-impl_iterator_obs!(i16, u16, 16);
-impl_iterator_obs!(i32, u32, 32);
 
 #[cfg(test)]
 mod tests {
@@ -206,46 +209,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn can_stream_empty() {
-        let mut obs: OutBitStream<core::slice::Iter<i8>, u8> = OutBitStream::new([].iter());
-        assert_eq!(obs.next(), None);
-    }
-    #[test]
     fn can_stream_u8() {
-        let mut obs = OutBitStream::new([0xAB_u8 as i8, 0xCD_u8 as i8].iter());
+        let mut obs = OutBitStream::new(0xAB_u8, 8);
         for i in 0..8 {
             assert_eq!(obs.next(), Some((0xAB & (1 << (7 - i))) != 0));
         }
-        for i in 0..8 {
-            assert_eq!(obs.next(), Some((0xCD & (1 << (7 - i))) != 0));
-        }
-
         assert_eq!(obs.next(), None);
     }
 
     #[test]
     fn can_stream_u16() {
-        let mut obs = OutBitStream::new([0xABCD_u16 as i16, 0xEF01_u16 as i16].iter());
+        let mut obs = OutBitStream::new(0xABCD_u16, 16);
         for i in 0..16 {
             assert_eq!(obs.next(), Some((0xABCD & (1 << (15 - i))) != 0));
         }
-        for i in 0..16 {
-            assert_eq!(obs.next(), Some((0xEF01 & (1 << (15 - i))) != 0));
-        }
-
         assert_eq!(obs.next(), None);
     }
 
     #[test]
     fn can_stream_u32() {
-        let mut obs = OutBitStream::new([0xABCD_EF01_u32 as i32, 0x2345_6789_u32 as i32].iter());
+        let mut obs = OutBitStream::new(0xABCD_EF01_u32, 32);
         for i in 0..32 {
             assert_eq!(obs.next(), Some((0xABCD_EF01_u32 & (1 << (31 - i))) != 0));
         }
-        for i in 0..32 {
-            assert_eq!(obs.next(), Some((0x2345_6789_u32 & (1 << (31 - i))) != 0));
-        }
-
         assert_eq!(obs.next(), None);
     }
 }
